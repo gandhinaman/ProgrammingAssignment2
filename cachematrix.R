e69bccbdec671396c42b38d36ec3832260c8f912
gandhinaman <naman.gandhi@gmail.com>
2016-10-31 13:41:22 +0530

    cachematrix.R
    Added a test case and few comments for easy evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
 ## ***************************************************** SCRIPT AUTHOR ****************************************************
-## Title - Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
+## Project - Coursera Data Science Specialization (JHU) - R Programming
+## Title - WEEK 3: Peer-graded Assignment: Programming Assignment 2: Lexical Scoping
 ## Author - Naman Gandhi
-## Last Modified Date - 10/30/2016
+## Last Modified Date - 10/31/2016
+## Version History - v1.1
+## Version Comments - Added a test case at the bottom of the script for easy evaluation
 ## ************************************************************************************************************************
 
 
 ## **************************************************** SCRIPT PURPOSE ****************************************************
 ## Matrix inversion is usually a costly computation and there may be some benefit to caching the inverse of a matrix 
-## rather than compute it repeatedly (there are also alternatives to matrix inversion that we will not discuss here). 
-## This assignment is to write a pair of functions that cache the inverse of a matrix.
+## rather than compute it repeatedly, this assignment is to write a pair of functions that cache the inverse of a matrix.
 
 ## Assumption: The matrix supplied is always invertible.
 ## ************************************************************************************************************************
@@ -16,17 +18,34 @@
 
 ## ************************************************************************************************************************
 ## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+## This function creates the special list containing four functions allowing to set and get the matrix as well as its stored 
+## inverse
 ## ************************************************************************************************************************
 
 makeCacheMatrix <- function(x = matrix()) {
-    m <- matrix(data=NA,nrow=100,ncol=100)
+    ## Initializing the inverse matrix to be NULL at the code beginning
+    Inverse <- NULL
+    
+    ## One alternative way to initialize the matrix would be using NAs using 'matrix' function
+    ## Inverse <- matrix(data=NA,nrow=100,ncol=100)
+    
+    ## If the matrix x is set a new value, then the Inverse is also set to NULL using the 'set' function
     set <- function(y) {
         x <<- y
-        m <<- NULL
+        Inverse <<- NULL
     }
+    
+    ## The 'get' funtcion returns our original matrix from the special list of 4 values
     get <- function() x
-    setinv <- function(inv) m <<- inv
-    getinv <- function() m
+    
+    ## The 'setinv' will replace the stored 'Inverse' matrix with the 'inv' matrix
+    ## <<- operator is used to assign a value to an object in an environment that is different from the current environment
+    setinv <- function(inv) Inverse <<- inv
+    
+    ## The 'getinv' will allow us to get the stored inverse matrix from our special list of 4 values
+    getinv <- function() Inverse
+    
+    ## Create the special list having 4 parameters
     list(set = set, get = get,
          setinv = setinv,
          getinv = getinv)
@@ -40,16 +59,36 @@ makeCacheMatrix <- function(x = matrix()) {
 ## ************************************************************************************************************************
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-    m <- x$getinv()
-    if(!all(is.na(m))) {
+    ## Use the getinv function of the special list to seek the Inverse matrix
+    Inverse <- x$getinv()
+    
+    ## Check if the returned matrix is NOT NULL, if so that means Inverse exists in cache and we will pick the cached entry
+    if(!is.null(Inverse)) {
         message("getting cached data")
-        return(m)
+        return(Inverse)
     }
-    data <- x$get()
-    m <- solve(data)
-    x$setinv(m)
-    m
+    ## (!all(is.na(Inverse))) will test the matrix for being NA if initialized by alternative way
+    
+    ## Executing the below set of commands will compute the Inverse using the Solve function
+    inputMat <- x$get()
+    Inverse <- solve(inputMat, ...)
+    x$setinv(Inverse)
+    
+    ## Return a matrix that is the inverse of 'x'
+    return(Inverse)
 }
+
+
+## ************************************************************************************************************************
+## A quick test case to help check the expected output
+## ************************************************************************************************************************
+
+matlist <- makeCacheMatrix(matrix(c(2, 4, 1, 5), nrow=2, ncol=2))
+invmat <- cacheSolve(matlist)
+## for the first occurence, you will get the matrix computed using the function written above
+invmat
+
+invmat <- cacheSolve(matlist)
+## for the second iteration, the inverse will be fetched from the cache and you will see "getting cached data" message
 
 ## ******************************************************** END ***********************************************************
